a1a5528e792196e55407a78d2f3d4e683108d38a
Alvin Kim <alvinkim101@gmail.com>
2024-11-23 19:51:44 -0800

    Add window abstraction and glfw

--- Filbert/src/Filbert/Core.h
+++ Filbert/src/Filbert/Core.h
@@ -9,3 +9,11 @@
 #else
 	#error "Filbert only supports Windows"
 #endif
+
+#ifdef FB_ASSERTS_ENABLED
+	#define FB_CORE_ASSERT(x, ...) if (!x) { FB_CORE_ERROR("Assertion failed: {}", __VA_ARGS__); __debugbreak(); }
+	#define FB_ASSERT(x, ...) if (!x) { FB_ERROR("Assertion failed: {}", __VA_ARGS__); __debugbreak(); }
+#else
+	#define FB_CORE_ASSERT(x, ...)
+	#define FB_ASSERT(x, ...)
+#endif
---
+++ Filbert/src/Platform/Windows/WindowsWindow.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Filbert/Window.h"
+
+struct GLFWwindow;
+
+namespace Filbert
+{
+	class WindowsWindow : public Window
+	{
+	public:
+		WindowsWindow(const WindowProps& props);
+		~WindowsWindow() override;
+
+		void OnUpdate() override;
+
+		void SetEventCallback(const EventCallbackFn& callback) override { m_data.callback = callback; }
+		void SetVSync(bool enabled) override;
+
+		bool IsVSyncEnabled() const override { return m_data.vSync; }
+		unsigned int GetWidth() const override { return m_data.width; }
+		unsigned int GetHeight() const override { return m_data.height; }
+
+	private:
+		virtual void Init(const WindowProps& props);
+		virtual void Shutdown();
+
+		struct WindowData
+		{
+			std::string title;
+			unsigned int width, height;
+			bool vSync;
+
+			EventCallbackFn callback;
+		};
+
+		WindowData m_data;
+		GLFWwindow* m_window;
+	};
+}
