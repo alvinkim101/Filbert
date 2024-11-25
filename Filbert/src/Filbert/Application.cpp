94bfcd0da1b9269749f0ca9033f3049b926898aa
Alvin Kim <alvinkim101@gmail.com>
2024-11-24 17:42:58 -0800

    Add Windows events

--- Filbert/src/Filbert/Application.cpp
+++ Filbert/src/Filbert/Application.cpp
@@ -2,9 +2,11 @@
 
 namespace Filbert
 {
+	#define BIND_EVENT_FN(f) std::bind(&Application::f, this, std::placeholders::_1)
+
 	Application::Application()
 	{
-
+		m_window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -18,5 +20,19 @@ namespace Filbert
 		{
 			m_window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& event)
+	{
+		EventDispatcher dispatcher(event);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowCloseEvent));
+
+		FB_CORE_TRACE("{}", event.Info());
+	}
+
+	bool Application::OnWindowCloseEvent(WindowCloseEvent& event)
+	{
+		m_running = false;
+		return true;
 	}
 }
