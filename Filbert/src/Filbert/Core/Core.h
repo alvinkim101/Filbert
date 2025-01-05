1dbf32db4bd0a8a8b3c966f4fed31dc5c9e1ffdc
Alvin Kim <alvinkim101@gmail.com>
2025-01-05 04:36:47 -0800

    Pass assert information directly into log

--- Filbert/src/Filbert/Core/Core.h
+++ Filbert/src/Filbert/Core/Core.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #ifdef FB_ASSERTS_ENABLED
-	#define FB_CORE_ASSERT(x, ...) if (!(x)) { FB_CORE_ERROR("Assertion failed: {}", __VA_ARGS__); __debugbreak(); }
-	#define FB_ASSERT(x, ...) if (!(x)) { FB_ERROR("Assertion failed: {}", __VA_ARGS__); __debugbreak(); }
+	#define FB_CORE_ASSERT(x, ...) if (!(x)) { FB_CORE_ERROR(__VA_ARGS__); __debugbreak(); }
+	#define FB_ASSERT(x, ...) if (!(x)) { FB_ERROR(__VA_ARGS__); __debugbreak(); }
 #else
 	#define FB_CORE_ASSERT(x, ...)
 	#define FB_ASSERT(x, ...)
